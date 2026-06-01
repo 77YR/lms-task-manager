c1f0c7357d17efd24a232698ab389944451f223f
Yafi Rahman <yafi.rahman0516@gmail.com>
2026-06-01 16:08:06 -0500

    Define app colors and currently locked to light theme

--- app/src/main/java/com/lmstaskmanager/app/ui/theme/Color.kt
+++ app/src/main/java/com/lmstaskmanager/app/ui/theme/Color.kt
@@ -2,10 +2,22 @@ package com.lmstaskmanager.app.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+// Brand
+val Blue600 = Color(0xFF2563EB)
+val Blue700 = Color(0xFF1D4ED8)
+val Blue100 = Color(0xFFDBEAFE)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+// Backgrounds
+val Gray50 = Color(0xFFF9FAFB)
+val Gray100 = Color(0xFFF3F4F6)
+val Gray200 = Color(0xFFE5E7EB)
+val Gray300 = Color(0xFFD1D5DB)
+val Gray400 = Color(0xFF9CA3AF)
+val Gray500 = Color(0xFF6B7280)
+val Gray900 = Color(0xFF111827)
+
+// Semantic
+val Red500 = Color(0xFFEF4444)
+val Green500 = Color(0xFF22C55E)
+val DarkGray = Color(0xFF828282)
+val White = Color(0xFFFFFFFF)
